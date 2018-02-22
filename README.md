0abd30c1505f2babbbc11265dd6bb3a50a058057
Golo Roden <golo.roden@thenativeweb.io>
2018-02-22 10:05:10 +0100

    Introduce async/await.

--- README.md
+++ README.md
@@ -1,66 +1,59 @@
-# seal-counter-storage
+# @sealsystems/counter-storage
 
 [![CircleCI](https://circleci.com/gh/sealsystems/seal-counter-storage.svg?style=svg)](https://circleci.com/gh/sealsystems/seal-counter-storage)
 [![AppVeyor](https://ci.appveyor.com/api/projects/status/nt7hj2kbbd6n2sm9?svg=true)](https://ci.appveyor.com/project/Plossys/seal-counter-storage)
 
-seal-counter-storage manages handling of counters persisted in a MongoDB.
+@sealsystems/counter-storage manages handling of counters persisted in a MongoDB.
 
 ## Installation
 
-```bash
-$ npm install seal-counter-storage
+```shell
+$ npm install @sealsystems/counter-storage
 ```
 
 ## Quick start
 
-First you need to add a reference to seal-counter-storage within your application.
+First you need to add a reference to @sealsystems/counter-storage within your application:
 
 ```javascript
-var counterStorage = require('seal-counter-storage');
+const counterStorage = require('@sealsystems/counter-storage');
 ```
 
 Then you can connect to a MongoDB. For that call the `connect` function and provide the database's connection string using the `url` property:
 
 ```javascript
-counterStorage.connect({ url: 'mongodb://localhost:27017' }, function (err, storage) {
-  // ...
-})
+const storage = await counterStorage.connect({
+  url: 'mongodb://localhost:27017'
+});
 ```
 
 Optionally, you may also specify the number of connection retries:
 
 ```javascript
-counterStorage.connect({
+const storage = await counterStorage.connect({
   url: 'mongodb://localhost:27017',
   connectionRetries: 10
-}, function (err, storage) {
-  // ...
-})
+});
 ```
 
 ## Get next counter value
 
-To get the next counter value call the `getCounter` function and provide at least its name and a callback.
-Optionally you can provide the increment as second argument, default increment is `1`.
+To get the next counter value call the `getCounter` function and provide its name. Optionally you can provide the increment as second argument, default increment is `1`:
 
 ```javascript
-storage.getCounter('hugo', function (err, newCounterValue) {
-  // ...
-});
+const newCounterValue = await storage.getCounter('hugo');
 ```
 
 With increment:
 
 ```javascript
-storage.getCounter('hugo', 100, function (err, newCounterValue) {
-  // ...
-});
+const newCounterValue = await storage.getCounter('hugo', 100);
 ```
 
 ## Running the build
 
 To build this module use [roboter](https://www.npmjs.com/package/roboter).
 
-```bash
+```shell
 $ bot
 ```
---
+++ test/StorageTest.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const assert = require('assertthat');
+const { host } = require('docker-host')();
+const uniq = require('lodash/uniq');
+const uuid = require('uuidv4');
+
+const mongo = require('@sealsystems/mongo');
+
+const counterStorage = require('../lib/counterStorage');
+const Storage = require('../lib/Storage');
+
+suite('Storage', () => {
+  test('is a function.', async () => {
+    assert.that(Storage).is.ofType('function');
+  });
+
+  test('throws an error when database is missing.', async () => {
+    assert.that(() => {
+      /* eslint-disable no-new */
+      new Storage();
+      /* eslint-enable no-new */
+    }).is.throwing('Database is missing.');
+  });
+
+  test('returns an object.', async () => {
+    const storage = new Storage({
+      collection () {}
+    });
+
+    assert.that(storage).is.ofType('object');
+  });
+
+  suite('getCounter', () => {
+    const dbName = uuid();
+    const url = `mongodb://${host}/${dbName}`;
+
+    let storage;
+
+    suiteSetup(async () => {
+      storage = await counterStorage.connect({ url });
+    });
+
+    suiteTeardown(async function () {
+      this.timeout(10000);
+
+      const db = await mongo.db(url, { connectionRetries: 1 });
+
+      await db.dropDatabase();
+    });
+
+    test('is a function.', async () => {
+      assert.that(storage.getCounter).is.ofType('function');
+    });
+
+    test('throws an error if the counter name is missing.', async () => {
+      await assert.that(async () => {
+        await storage.getCounter();
+      }).is.throwingAsync('Counter name is missing.');
+    });
+
+    test('returns next ref number', async () => {
+      const refNo = await storage.getCounter('gcTest');
+
+      assert.that(refNo).is.not.null();
+    });
+
+    test('ref numbers are incremented by one per default', async () => {
+      const refNo1 = await storage.getCounter('gcTest');
+      const refNo2 = await storage.getCounter('gcTest');
+
+      assert.that(refNo2).is.equalTo(refNo1 + 1);
+    });
+
+    test('ref numbers are incremented by given increment', async () => {
+      const refNo1 = await storage.getCounter('gcTest');
+      const refNo2 = await storage.getCounter('gcTest', 100);
+
+      assert.that(refNo2).is.equalTo(refNo1 + 100);
+    });
+
+    test('get 10000 unique numbers', async function () {
+      this.timeout(10 * 1000);
+
+      const promises = [];
+
+      for (let i = 0; i < 10000; i++) {
+        promises.push(storage.getCounter('massCounter'));
+      }
+
+      const results = await Promise.all(promises);
+      const distinct = uniq(results);
+
+      assert.that(results.length).is.equalTo(10000);
+      assert.that(results).is.equalTo(distinct);
+    });
+  });
+});
